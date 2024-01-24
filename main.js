3a0b170c1603b7c1a13b2fae603d3a924c544db9
memirkocak <mekocak@edenschool.fr>
2024-01-24 13:02:40 +0100

    final version

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 
+let inputname = document.querySelector('.name-input');
+let email = document.querySelector('.email-input');
+let message = document.querySelector('.message-input');
+let btn = document.querySelector('#btn');
+let arr = ['aaaaa'];
+
+
+
+function btndisable(){
+    if(inputname.textContent > arr){
+        
+    }
+}
+
+btn.addEventListener('click',btndisable)
